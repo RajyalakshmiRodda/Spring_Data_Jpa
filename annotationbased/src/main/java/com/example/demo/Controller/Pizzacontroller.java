21cf7d4e8ab2f3549079533494b024d97d66297b
RajyalakshmiRodda <roddarajyalakshmi8@gmail.com>
2023-11-03 15:31:37 +0530

    upload annotation based projct.

---
+++ annotationbased/src/main/java/com/example/demo/Controller/Pizzacontroller.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Pizzacontroller {
+	public String getpizza() {
+		return "i want chicken pizza";
+	}
+
+}
